d3d1a62739c05525e60926b7f10a8265d0fa1a8f
Teddi <tedthedude27@gmail.com>
2021-06-14 16:51:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,6 @@ function getWeather() {
         location.innerHTML = data.name;
         let humidity = data.main.humidity;
         description.innerHTML = "Description: " + data.weather[0].description + ", Humidity: " + humidity + ", Wind Speed: " + data.wind.speed + " mph";
-        document.write(<img scr="http://openweathermap.org/img/wn/10d@2x.png"/>)
       });
   }
 
