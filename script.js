df88bb53bd289f081da81ca59cc26b602fbb259e
Teddi <83455454+teddinotteddy@users.noreply.github.com>
2021-06-13 21:10:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
 function getWeather() {
   let temperature = document.getElementById("temperature");
   let description = document.getElementById("description");
