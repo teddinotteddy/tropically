92ab0ad0d4e1eb29abb2bd78529100d1ae916a20
Teddi <83455454+teddinotteddy@users.noreply.github.com>
2021-06-13 21:12:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
+date.innerHTML = dateTime;
 
 function getWeather() {
   let temperature = document.getElementById("temperature");
