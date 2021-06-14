c416826366b4320fb96799ba76a827e02f518b9b
Teddi <83455454+teddinotteddy@users.noreply.github.com>
2021-06-13 21:34:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var today = new Date();
 var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var time = today.getHours() + ":" + today.getMinutes()
 var dateTime = date+' - '+time;
 timing.innerHTML = dateTime;
 
