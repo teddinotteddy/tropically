6cd33fad907a23f6e1e266786084bd10a887b31f
Teddi <tedthedude27@gmail.com>
2021-06-16 17:01:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function getColor() {
+  return (
+    "#" +
+    Math.random()
+      .toString(16)
+      .slice(2, 8)
+  );
+}
+
+function setBackground() {
+  var bgColor = getColor();
+  document.body.style.background = bgColor;
+}
+
+window.onload(setBackground())
+
 function getTime() {
   var today = new Date();
   var now = today.getDay();
