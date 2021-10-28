4c7785ada9a32729895ff72c35d6bf235126d679
Teddi <83455454+teddinotteddy@users.noreply.github.com>
2021-10-28 10:18:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ window.onload = function() {
   visitCount();
 };
 
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
+console.log(strTime)
+
 function getTime() {
   var today = new Date();
   var now = today.getDay();
